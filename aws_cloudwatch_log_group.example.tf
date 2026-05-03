9ca4e4e7859fa3ac16b7690e17e771389ac7eef8
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 19:10:07 +0100

    fix: CKV_AWS_338 set log retention to 365 days

--- aws_cloudwatch_log_group.example.tf
+++ aws_cloudwatch_log_group.example.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_group" "examplea" {
   name              = "/aws/directoryservice/${aws_directory_service_directory.ad.id}"
-  retention_in_days = 14
+  retention_in_days = 365
   kms_key_id        = var.kms_key_id
   tags              = var.common_tags
 }
